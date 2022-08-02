1b5d0571ef8178044e1b746d566920f6b161e562
DarkFighterLuke <luca-consoli@live.it>
2022-08-02 16:27:55 +0200

    Add column name tag to a field

--- models/traccia.go
+++ models/traccia.go
@@ -1,7 +1,7 @@
 package models
 
 type Traccia struct {
-	TracciaId     uint   `gorm:"primaryKey" json:"tracciaId"`
+	TracciaId     uint   `gorm:"primaryKey;column:tracce_id" json:"tracciaId"`
 	NumeroTraccia int    `json:"numeroTraccia"`
 	Titolo        string `json:"titolo"`
 	Durata        int    `json:"durata"`
